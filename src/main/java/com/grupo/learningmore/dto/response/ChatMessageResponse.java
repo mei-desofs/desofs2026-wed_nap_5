7cb96de20a129b3170d110a7a9216e0fbab63a56
1250512 <1250512@isep.ipp.pt>
2026-06-11 17:24:17 +0100

    #29 fix:chat message response

--- src/main/java/com/grupo/learningmore/dto/response/ChatMessageResponse.java
+++ src/main/java/com/grupo/learningmore/dto/response/ChatMessageResponse.java
@@ -1,10 +1,34 @@
 package com.grupo.learningmore.dto.Response;
 
-import java.util.Date;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public record ChatMessageResponse(
-        String id,
-        String content,
-        Date sentAt
-) {
+import java.util.Date;
+import java.util.UUID;
+
+public class ChatMessageResponse {
+
+    private UUID id;
+    @NotBlank
+    @Size(max = 1000)
+    private String content;
+    private Date sentAt;
+
+    public ChatMessageResponse(UUID id, String content, Date sentAt) {
+        this.id = id;
+        this.content = content;
+        this.sentAt = sentAt;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Date getSentAt() {
+        return sentAt;
+    }
 }
